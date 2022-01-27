de8be7a711d63ca4e5ce3d217842473422e10c6d
sjoongh <az45687@naver.com>
2022-01-27 16:47:37 +0900

    main

---
+++ src/quiz/Main.java
@@ -0,0 +1,26 @@
+package quiz;
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] num = new int[n];
+        for (int i = 0; i < n; i++) {
+            num[i] = sc.nextInt();
+        }
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+        	int j = 2;
+            for (j = 2; j < num[i]; j++) {
+                if (num[i] % j == 0)
+                    break;
+            }
+            if (j == num[i]){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
