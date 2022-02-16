d2b857a8e334b3aed78d33e020c08cdd3ab4ae62
sjoongh <az45687@naver.com>
2022-02-16 12:24:38 +0900

    network2

--- src/day15_network/ex02/net/URLEx.java
+++ src/day15_network/ex02/net/URLEx.java
@@ -7,17 +7,17 @@ public class URLEx {
 
 	public static void main(String[] args) 
 			throws MalformedURLException, IOException {
-		URL google = new URL("http://www.google.com"); // 예외 발생
 		
-		BufferedReader br = new BufferedReader(new InputStreamReader(google.openStream())); // 예외발생
+		URL url = new URL("http://ec2-3-34-126-3.ap-northeast-2.compute.amazonaws.com/"); // 예외 발생
 		
-		String inputLine;
-		
-		while ( (inputLine = br.readLine() ) != null) {
-			System.out.println(inputLine);
-		}
-		
-		br.close();
+		System.out.println("프로토콜 : "+url.getProtocol());
+		System.out.println("호스트와 포트 : "+url.getAuthority());
+		System.out.println("호스트 : "+url.getHost());
+		System.out.println("포트 : "+url.getPort());
+		System.out.println("경로 : "+url.getPath());
+		System.out.println("질의 : "+url.getQuery());
+		System.out.println("파일명 : "+url.getFile());
+		System.out.println("참조 : "+url.getRef());
 	}
 
 }
